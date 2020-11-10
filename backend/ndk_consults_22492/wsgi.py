dcde374e7e6d84dc09e016c1f5251ad128378c5f
Crowdbotics <team@crowdbotics.com>
2020-11-10 21:27:25 +0000

    Update models

--- backend/ndk_consults_22492/wsgi.py
+++ backend/ndk_consults_22492/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ndk_consults_22492.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ndk_consults_22492.settings")
 
 application = get_wsgi_application()
